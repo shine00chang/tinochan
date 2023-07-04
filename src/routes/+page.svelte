2461f4db095efa24640e436369f97177ee2d366a
Shine Chang <shine00chang@gmail.com>
2023-07-04 10:09:11 -0700

    (impl) basic ssr app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    // States
+    let lucky_text;
+
+    async function onclick () {
+        const response = await fetch ("/api/test");
+        lucky_text = await response.json();
+    }
+</script>
+
+<div> 
+    <p>Get your lucky number from svelte's server!</p>
+    <button on:click={onclick}>Claim Now!</button><br>
+
+    {#if lucky_text != undefined} 
+        Tada:
+        <p>{lucky_text}<p>
+    {/if}
+</div>
---
+++ src/routes/api/test/+server.js
@@ -0,0 +1,8 @@
+import { json } from '@sveltejs/kit';
+
+
+export function GET() {
+	const number = Math.floor(Math.random() * 6) + 1;
+
+	return json(`here's your lucky number: ${number}`);
+}
