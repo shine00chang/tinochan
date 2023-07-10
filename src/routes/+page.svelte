504c1550b0c5a7835d4870e9e4fd484a511d2b4a
Shauryan  K <shauryan.kanaujia@gmail.com>
2023-07-09 17:31:35 -0700

    Go to -> Enter (Wording)
    Just testing out the process of updating the repository with this change.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 <div class="font-bold text-4xl underline decoration-sky-500"> Tinochan </div>
 <br>
 
-<button class="hover-1" on:click={_ => goto("/feed")}>Go to Tinochan's secret stash!</button>
+<button class="hover-1" on:click={_ => goto("/feed")}>Enter Tinochan's secret stash!</button>
 <p> (You need to log in first) </p>
