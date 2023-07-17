10187097250053801ceea4f40bd2b6169b5ff31f
Shauryan  K <shauryan.kanaujia@gmail.com>
2023-07-16 17:30:46 -0700

    quickfix: login required
    changed so that login required only appears if user is not logged in.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
-    import { goto } from '$app/navigation';
+    import { goto, invalidateAll } from '$app/navigation';
+
+    export let data
 </script>
 
 <img src="/bannerv4.png" alt="Banner" class="mx-auto">
@@ -9,7 +11,7 @@
     <!--<div class="font-bold text-8xl underline decoration-sky-500"> Tinochan </div> -->
     
     <button class="text-3xl hover-1" on:click={_ => goto("/feed")}>→Enter Forum←</button> 
-   
-    <p> (You need to log in first) </p>
-
+    {#if !data.session_cookie}
+    <p class="text-red-500"> (login required) </p>
+    {/if}
 </div>
